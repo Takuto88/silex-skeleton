254e8e832c8207ac236a6f3ee7e88f52c04b6fed
Lennart Rosam <hello@takuto.de>
2015-12-21 22:09:05 +0100

    Add better description

--- resources/i18n/de.php
+++ resources/i18n/de.php
@@ -4,5 +4,7 @@ return array (
 		'words.go' => 'Los',
 		'message.greeting' => 'Dies ist eine Silex Demoapplikation. Siehe in die README.md für details.',
 		'message.whatsyourname' => 'Wie heißt du?',
-		'message.fetchMessages' => 'Messages holen!'
+		'message.fetchMessages' => 'Messages holen!',
+		'message.rest.generalDescription' => 'Dieses Skeleton hat neben diesem HTML-Controller auch einen Rest-Controller als Beispiel (MessageController). Hier kannst du ihn testen. Siehe messages.js für den JS-Client, index.php für das Routing und MessageController.php für den Controller.',
+		'message.rest.indexDescription' => 'Diese Ressoure liest alle Messages aus der Datenbank.'
 );
--- resources/i18n/en.php
+++ resources/i18n/en.php
@@ -4,5 +4,7 @@ return array (
 		'words.go' => 'Go',
 		'message.greeting' => 'This is a Silex demo application. See README.md for more details.',
 		'message.whatsyourname' => 'What\'s your name?',
-		'message.fetchMessages' => 'Fetch messages!'
+		'message.fetchMessages' => 'Fetch messages!',
+		'message.rest.generalDescription' => 'This skeleton has not only this HTML-Controller but also a Rest-Controller example (MessageController). You can test it below. See messages.js for JS-Client, index.php for Routing and MessageController.php for the Controller.',
+		'message.rest.indexDescription' => 'This resource retrieves all Messages from the database.'
 );
