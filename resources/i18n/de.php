6b2c7f7d059cfc57a72fa63f8450e1832499c1e4
Lennart Rosam <hello@takuto.de>
2015-12-21 23:23:02 +0100

    Add language keys + GET interface

--- resources/i18n/de.php
+++ resources/i18n/de.php
@@ -2,9 +2,16 @@
 return array (
 		'words.hello' => 'Hallo',
 		'words.go' => 'Los',
+		
+		'exception.msg.entityNotFound' => 'Objekt nicht gefunden',
+		
 		'message.greeting' => 'Dies ist eine Silex Demoapplikation. Siehe in die README.md für details.',
+		'message.oops' => 'Ups, da ist was schief gegangen.',
+		'message.error.code' => 'Fehlercode %code% - %message%',
 		'message.whatsyourname' => 'Wie heißt du?',
 		'message.fetchMessages' => 'Messages holen!',
+		'message.fetchMessage' => 'Message holen!',
 		'message.rest.generalDescription' => 'Dieses Skeleton hat neben diesem HTML-Controller auch einen Rest-Controller als Beispiel (MessageController). Hier kannst du ihn testen. Siehe messages.js für den JS-Client, index.php für das Routing und MessageController.php für den Controller.',
-		'message.rest.indexDescription' => 'Diese Ressoure liest alle Messages aus der Datenbank.'
+		'message.rest.indexDescription' => 'Diese Ressoure liest alle Messages aus der Datenbank.',
+		'message.rest.getDescription' => 'Diese Ressource holt eine gezielte Message aus der Datenbank.',
 );
--- resources/i18n/en.php
+++ resources/i18n/en.php
@@ -2,9 +2,16 @@
 return array (
 		'words.hello' => 'Hello',
 		'words.go' => 'Go',
+		
+		'exception.msg.entityNotFound' => 'Object not found!',
+		
 		'message.greeting' => 'This is a Silex demo application. See README.md for more details.',
+		'message.oops' => 'Oops! Something went wrong.',
+		'message.error.code' => 'Error %code% - %message%',
 		'message.whatsyourname' => 'What\'s your name?',
 		'message.fetchMessages' => 'Fetch messages!',
+		'message.fetchMessage' => 'Fetch message!',
 		'message.rest.generalDescription' => 'This skeleton has not only this HTML-Controller but also a Rest-Controller example (MessageController). You can test it below. See messages.js for JS-Client, index.php for Routing and MessageController.php for the Controller.',
-		'message.rest.indexDescription' => 'This resource retrieves all Messages from the database.'
+		'message.rest.indexDescription' => 'This resource retrieves all Messages from the database.',
+		'message.rest.getDescription' => 'This resource retrieves a single message by ID',
 );
